1424b5dc4237fb06dc9045beca3906d80c62ec02
Mouhamad Djigo <mehmetdjigo@gmail.com>
2025-02-18 12:34:07 +0000

    first commit

---
+++ src/main/java/com/jgotechmaker/debut/DebutApplication.java
@@ -0,0 +1,13 @@
+package com.jgotechmaker.debut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgotechmaker/debut/DebutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgotechmaker.debut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
